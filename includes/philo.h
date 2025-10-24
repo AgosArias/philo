85397cb4eab8f0e5aa1cf82f428708134aba786c
agosarias <agosarias99@gmail.com>
2025-10-24 19:31:59 +0200

    add: Add folder include and the file .h

---
+++ includes/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarias-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 19:25:33 by aarias-d          #+#    #+#             */
+/*   Updated: 2025/10/24 19:30:23 by aarias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <studio.h>
+
+typedef struct s_philo
+{
+	int	eat;
+}	d_philo;
+
+typedef struct s_params
+{
+	int	num_philo;
+}	d_params;
+
+#endif
+
